ec13f58feea6247fdfa3fd51085769506468a6ba
Anthony Mace <amace93@gmail.com>
2017-01-08 15:52:55 -0700

    script for firebase setup

---
+++ JS/firebaseSetup.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAyRPw1Vn4yel8vHbk0pX7PZrxb9b0jYDQ",
+    authDomain: "emilyrobinsonphotography.firebaseapp.com",
+    databaseURL: "https://emilyrobinsonphotography.firebaseio.com",
+    storageBucket: "emilyrobinsonphotography.appspot.com",
+    messagingSenderId: "504310803869"
+};
+firebase.initializeApp(config);
